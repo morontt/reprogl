4f92adc6dd87e82fdd503f06b460bcae9bd9910c
Alexander Harchenko <morontt@gmail.com>
2019-03-05 00:12:15 +0200

    use FastCGI

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"log"
+	"net"
 	"net/http"
+	"net/http/fcgi"
 	"reprogl/controllers"
 )
 
 func main() {
 	http.HandleFunc("/", controllers.Index)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	listener, _ := net.Listen("tcp", ":8080")
+	fcgi.Serve(listener, nil)
 }
