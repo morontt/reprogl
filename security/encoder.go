180e63a3d68288d69a9df3e4df7e5bff1ae0a55c
Alexander Harchenko <morontt@gmail.com>
2019-04-30 19:52:59 +0300

    small optimization

--- security/encoder.go
+++ security/encoder.go
@@ -8,16 +8,16 @@ import (
 const iterations = 4000
 
 func EncodePassword(password string, salt string) string {
-	salted := password + "{" + salt + "}"
+	salted := []byte(password + "{" + salt + "}")
 	h := sha512.New384()
 
-	h.Write([]byte(salted))
+	h.Write(salted)
 	digest := h.Sum(nil)
 
 	for i := 1; i < iterations; i++ {
 		h.Reset()
 		h.Write(digest)
-		h.Write([]byte(salted))
+		h.Write(salted)
 		digest = h.Sum(nil)
 	}
 
