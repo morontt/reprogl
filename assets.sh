33546fc0d1d60adfbed5f6c7a3fb3c49c59a51a9
Alexander Harchenko <morontt@gmail.com>
2023-05-31 17:04:37 +0300

    Trick for browser cache

--- assets.sh
+++ assets.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 GRUNTCMD="grunt"
+CURRENTDATE=$(date '+%y%j%H%M%S')
+
+sed -i 's/reprogl\.min\.v\([0-9]\+\)\.css/reprogl\.min\.v'$CURRENTDATE'\.css/' ./templates/layout/base.gohtml
+sed -i 's/reprogl\.min\.v\([0-9]\+\)\.js/reprogl\.min\.v'$CURRENTDATE'\.js/' ./templates/layout/base.gohtml
 
 while getopts ":s" opt; do
   case $opt in
