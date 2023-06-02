94116ea14b032e5f9f0298f4884d3636cfa3298b
Alexander Harchenko <morontt@gmail.com>
2023-06-02 14:53:47 +0300

    extension for assets #20

--- views/assets.go
+++ views/assets.go
@@ -3,7 +3,12 @@ package views
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
+	"html/template"
 	"os"
+	"strconv"
+	"strings"
+
 	"xelbot.com/reprogl/container"
 )
 
@@ -17,13 +22,46 @@ func cdnBase() string {
 	return cfg.CDNBaseURL
 }
 
+func assetTag(file string) template.HTML {
+	var (
+		tmpl    string
+		result  string
+		subPath string
+		ext     string
+	)
+
+	result, ok := fileHashes[file]
+	if ok {
+		return template.HTML(result)
+	}
+
+	if strings.HasSuffix(file, ".css") {
+		tmpl = `<link rel="stylesheet" href="%s" integrity="%s" crossorigin="anonymous">`
+		subPath = file[:len(file)-3]
+		ext = ".css"
+	} else if strings.HasSuffix(file, ".js") {
+		tmpl = `<script src="%s" integrity="%s" crossorigin="anonymous"></script>`
+		subPath = file[:len(file)-2]
+		ext = ".js"
+	} else {
+		tmpl = `<unknown src="%s" integrity="%s"/>`
+		subPath = file
+		ext = ".?"
+	}
+
+	hash := subresourceIntegrity(file)
+	timeBasedPart := timeBased(file)
+
+	result = fmt.Sprintf(tmpl, cfg.CDNBaseURL+"/"+subPath+timeBasedPart+ext, hash)
+	if !container.IsDevMode() {
+		fileHashes[file] = result
+	}
+
+	return template.HTML(result)
+}
+
 func subresourceIntegrity(file string) string {
 	var n int
-
-	h0, ok := fileHashes[file]
-	if ok {
-		return "sha256-" + h0
-	}
 
 	f, err := os.Open("./public/" + file)
 	if err != nil {
@@ -43,10 +81,29 @@ func subresourceIntegrity(file string) string {
 		hash.Write(bytes[:n])
 	}
 
-	h1 := base64.StdEncoding.EncodeToString(hash.Sum(nil))
-	if !container.IsDevMode() {
-		fileHashes[file] = h1
+	return "sha256-" + base64.StdEncoding.EncodeToString(hash.Sum(nil))
+}
+
+func timeBased(file string) string {
+	fileInfo, err := os.Stat("./public/" + file)
+	if err != nil {
+		return "v999"
 	}
 
-	return "sha256-" + h1
+	modificatedAt := fileInfo.ModTime()
+
+	return fmt.Sprintf(
+		"v%d%s%s%s%s",
+		modificatedAt.Year()%100,
+		zeroPad(modificatedAt.YearDay(), 3),
+		zeroPad(modificatedAt.Hour(), 2),
+		zeroPad(modificatedAt.Minute(), 2),
+		zeroPad(modificatedAt.Second(), 2),
+	)
+}
+
+func zeroPad(i, width int) string {
+	istring := strconv.Itoa(i)
+
+	return strings.Repeat("0", width-len(istring)) + istring
 }
