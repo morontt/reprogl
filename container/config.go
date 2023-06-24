b3855cae1db92e303916ca533d91b8252a00c210
Alexander Harchenko <morontt@gmail.com>
2023-06-24 15:17:37 +0300

    config refactoring #13

--- container/config.go
+++ container/config.go
@@ -2,55 +2,39 @@ package container
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
+	"reflect"
 
 	"github.com/gookit/ini/v2"
 )
 
-// TODO Auto read config by parameter type and name
-
 type AppConfig struct {
-	CDNBaseURL        string
-	DatabaseDSN       string
-	HeaderText        string
-	Host              string
-	Port              string
-	Author            string
-	AuthorBio         string
-	AuthorGithub      string
-	AuthorTelegram    string
-	AdminEmail        string
-	BackendApiUrl     string
-	BackendApiUser    string
-	BackendApiWsseKey string
-	TelegramToken     string
-	TelegramAdminID   int
+	CDNBaseURL        string `app_config:"CDN_BASE_URL"`
+	DatabaseDSN       string `app_config:"DB"`
+	HeaderText        string `app_config:"HEADER_TEXT"`
+	Host              string `app_config:"HOST"`
+	Port              string `app_config:"PORT"`
+	Author            string `app_config:"AUTHOR"`
+	AuthorBio         string `app_config:"AUTHOR_BIO"`
+	AuthorGithub      string `app_config:"AUTHOR_GITHUB"`
+	AuthorTelegram    string `app_config:"AUTHOR_TELEGRAM"`
+	AdminEmail        string `app_config:"ADMIN_EMAIL"`
+	BackendApiUrl     string `app_config:"BACKEND_API_URL"`
+	BackendApiUser    string `app_config:"BACKEND_API_USER"`
+	BackendApiWsseKey string `app_config:"BACKEND_API_WSSE_KEY"`
+	TelegramToken     string `app_config:"TELEGRAM_TOKEN"`
+	TelegramAdminID   int    `app_config:"TELEGRAM_ADMIN_ID"`
 }
 
 var cnf AppConfig
 
 func init() {
-	err := ini.LoadExists("app.ini")
+	err := cnf.load("app.ini")
 	if err != nil {
 		handleError(err)
 	}
-
-	cnf.Port = configStringValue("PORT")
-	cnf.DatabaseDSN = configStringValue("DB")
-	cnf.Host = configStringValue("HOST")
-	cnf.HeaderText = configStringValue("HEADER_TEXT")
-	cnf.CDNBaseURL = configStringValue("CDN_BASE_URL")
-	cnf.Author = configStringValue("AUTHOR")
-	cnf.AdminEmail = configStringValue("ADMIN_EMAIL")
-	cnf.AuthorBio = configStringValue("AUTHOR_BIO")
-	cnf.AuthorGithub = configStringValue("AUTHOR_GITHUB")
-	cnf.AuthorTelegram = configStringValue("AUTHOR_TELEGRAM")
-	cnf.BackendApiUrl = configStringValue("BACKEND_API_URL")
-	cnf.BackendApiUser = configStringValue("BACKEND_API_USER")
-	cnf.BackendApiWsseKey = configStringValue("BACKEND_API_WSSE_KEY")
-	cnf.TelegramToken = configStringValue("TELEGRAM_TOKEN")
-	cnf.TelegramAdminID = configIntValue("TELEGRAM_ADMIN_ID")
 }
 
 func GetConfig() AppConfig {
@@ -94,4 +78,63 @@ func configIntValue(paramName string) (value int) {
 	}
 
 	return
+}
+
+type structField struct {
+	name          string
+	parameterName string
+	fieldType     reflect.Kind
+}
+
+func (c *AppConfig) load(file string) error {
+	err := ini.LoadExists(file)
+	if err != nil {
+		return err
+	}
+
+	rv := reflect.ValueOf(c)
+	if rv.IsNil() {
+		return fmt.Errorf("config: load error %v", reflect.TypeOf(c))
+	}
+
+	fields := parseFields(reflect.TypeOf(*c))
+
+	confValue := rv.Elem()
+	for _, field := range fields {
+		f := confValue.FieldByName(field.name)
+		if f.IsValid() && f.CanSet() {
+			switch field.fieldType {
+			case reflect.Int:
+				x := int64(configIntValue(field.parameterName))
+				if !f.OverflowInt(x) {
+					f.SetInt(x)
+				} else {
+					return fmt.Errorf("config: int overflow for field %s", field.name)
+				}
+			case reflect.String:
+				f.SetString(configStringValue(field.parameterName))
+			default:
+				return fmt.Errorf("config: undefined type %s of field %s", field.fieldType, field.name)
+			}
+		}
+	}
+
+	return nil
+}
+
+func parseFields(t reflect.Type) []structField {
+	var fields = make([]structField, 0)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		tag := f.Tag.Get("app_config")
+		if tag != "" {
+			fields = append(fields, structField{
+				parameterName: tag,
+				fieldType:     f.Type.Kind(),
+				name:          f.Name,
+			})
+		}
+	}
+
+	return fields
 }
