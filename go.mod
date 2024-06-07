69b0862b5ef3b475b79a24aae006366ff7a80a04
Alexander Kharchenko <morontt@yandex.ru>
2024-06-07 13:30:19 +0300

    update vendors and more parallelism

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/speps/go-hashids/v2 v2.0.1
 	github.com/xelbot/yetacache v0.9.1
-	golang.org/x/image v0.15.0
+	golang.org/x/image v0.17.0
 )
 
 require (
@@ -18,8 +18,8 @@ require (
 	github.com/gookit/goutil v0.6.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- utils/avatar/gravatar.go
+++ utils/avatar/gravatar.go
@@ -7,8 +7,8 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"net/http"
+	"runtime"
 	"strings"
-	"sync"
 	"time"
 
 	"xelbot.com/reprogl/container"
@@ -17,8 +17,12 @@ import (
 
 var (
 	GravatarNotFound = errors.New("gravatar: not found")
-	locker           sync.Mutex
+	chanBuf          chan struct{}
 )
+
+func init() {
+	chanBuf = make(chan struct{}, (runtime.NumCPU()/2)+1)
+}
 
 func tryGravatar(commentator *models.CommentatorForGravatar) (image.Image, error) {
 	if !commentator.Email.Valid || !commentator.FakeEmail.Valid || commentator.FakeEmail.Bool {
@@ -64,8 +68,8 @@ func gravatarHash(s string) string {
 }
 
 func sendRequest(req *http.Request) (*http.Response, error) {
-	locker.Lock()
-	defer locker.Unlock()
+	chanBuf <- struct{}{}
+	defer func() { <-chanBuf }()
 
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
 	client := http.Client{
