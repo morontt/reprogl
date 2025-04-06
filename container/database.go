159c42224ccb8cc0ef933bd74f0208a0ba8c229e
Alexander Kharchenko <morontt@yandex.ru>
2025-04-06 21:56:50 +0300

    Set maximum of open connections
    https://www.alexedwards.net/blog/configuring-sqldb

--- container/database.go
+++ container/database.go
@@ -49,6 +49,10 @@ func openDB(dsn string) (*sql.DB, error) {
 		return nil, err
 	}
 
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
 	if err = db.Ping(); err != nil {
 		return nil, err
 	}
